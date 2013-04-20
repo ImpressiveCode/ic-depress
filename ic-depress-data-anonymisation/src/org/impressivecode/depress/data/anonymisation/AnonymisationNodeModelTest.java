c95a96eb1116fdd55de7ea4286de4f4a9ced2e88
Andrzej Dudek <andrzej@bigbee.eu>
2013-04-20 21:37:23 +0200

    tdd - pierwszy test jednostkowy

---
+++ ic-depress-data-anonymisation/src/org/impressivecode/depress/data/anonymisation/AnonymisationNodeModelTest.java
@@ -0,0 +1,14 @@
+package org.impressivecode.depress.data.anonymisation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnonymisationNodeModelTest {
+
+	@Test
+	public void test() {
+		assertTrue("CI - testing if tests works", true);
+	}
+
+}
