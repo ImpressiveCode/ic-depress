a10ce4ca0a45d0fa138672ad757d5aa638aea20b
Andrzej Dudek <andrzej@bigbee.eu>
2013-05-03 00:01:25 +0200

    unit tests for filehelper class

---
+++ ic-depress-data-anonymisation/src/org/impressivecode/depress/data/objects/FileHelperTest.java
@@ -0,0 +1,66 @@
+package org.impressivecode.depress.data.objects;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileHelperTest {
+
+    /**
+     * Checks if invalid filename will generate filename according to default settings.
+     */
+    @Test
+    public void testCreateTmpFileInvalidFilename() {
+        String expected = FileHelper.TMP_DIR + FileHelper.DIR_SEPARATOR
+                + FileHelper.KEY_FILENAME + "-" + 0 + "." + FileHelper.KEY_FILENAME_EXT;
+        assertEquals(FileHelper.CreateTmpFile(""), expected);
+    }
+
+    /**
+     * Checks if valid filename will generate filename according to default settings.
+     */
+    @Test
+    public void testCreateTmpFileValidFilename() {
+        String fname = "validName";
+        String expected = FileHelper.TMP_DIR + FileHelper.DIR_SEPARATOR
+                + fname + "-" + 0 + "." + FileHelper.KEY_FILENAME_EXT;
+        assertEquals(FileHelper.CreateTmpFile(fname), expected);
+    }
+
+    /**
+     * Checks if valid filename will generate filename with iterated number
+     * according to default settings.
+     */
+    @Test
+    public void testCreateTmpFileSameValidFilenames() throws IOException {
+        String fname = "validName";
+        String expected = FileHelper.TMP_DIR + FileHelper.DIR_SEPARATOR
+                + fname + "-" + 1 + "." + FileHelper.KEY_FILENAME_EXT;
+        String fpath = FileHelper.CreateTmpFile(fname);
+        File f = new File(fpath);
+        f.createNewFile();
+        fpath = FileHelper.CreateTmpFile(fname);
+        assertEquals(fpath, expected);
+        f.delete();
+    }
+
+    @Test
+    public void testWriteToFile() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGenerateKeyFile() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReadFromFile() {
+        fail("Not yet implemented");
+    }
+
+}
