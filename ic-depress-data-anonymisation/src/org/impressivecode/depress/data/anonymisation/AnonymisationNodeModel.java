2b9ffa7db5d30701a4c40e854f23800d4ee3c683
Andrzej Dudek <andrzej@bigbee.eu>
2013-05-04 20:37:21 +0200

    Merge branch 'dev' of https://github.com/Fizgon/ic-depress into merge-branch

--- ic-depress-data-anonymisation/src/org/impressivecode/depress/data/anonymisation/AnonymisationNodeModel.java
+++ ic-depress-data-anonymisation/src/org/impressivecode/depress/data/anonymisation/AnonymisationNodeModel.java
@@ -2,12 +2,22 @@ package org.impressivecode.depress.data.anonymisation;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.impressivecode.depress.data.objects.PropertiesValidator;
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.RowKey;
+import org.knime.core.data.container.ContainerTable;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
@@ -16,6 +26,11 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.workflow.BufferedDataTableView;
+
+import sun.io.Converters;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 /**
  * This is the model implementation of Anonymisation. Encrypts and decrypts selected 
@@ -48,31 +63,65 @@ public class AnonymisationNodeModel extends NodeModel {
     @Override
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec)
             throws Exception {
-
+        
         // TODO: Return a BufferedDataTable for each output port
-
+        DataTableSpec spec = new DataTableSpec();
+        BufferedDataContainer container = exec.createDataContainer(spec);
+        
+        //create a list to hold DataColumnsSpec 
+         DataColumnSpec[] tableDataColumnsSpec = new DataColumnSpec[inData.length];
+         //variable to get number of iteration(columns and rows)
+        int i=0;
+        int j=0;
         for(BufferedDataTable table : inData){
+            
             Iterator<DataColumnSpec> columnsItarator = table.getDataTableSpec().iterator(); 
+          //Create a DataTableSpec and BufferedDataContainer, there are two columns (it's a demo) ??  
+           
+
             while(columnsItarator.hasNext())
-            {
+            {   
+                //adding the specyfication of aktual columns that is iterate
+               // tableDataColumnsSpec.add(new DataColumnSpecCreator(columnsItarator.toString(), StringCell.TYPE).createSpec());
+                
               //TODO
                 //iterating every column. Should iterate only those from NodeDialog.ColumnFilter.
                 //Other just add to output. (but how?)
                 DataColumnSpec columnSpec = columnsItarator.next();
                 for(DataCell cell : columnSpec.getDomain().getValues())
                 {
+                    j++;
+                    //adding the custom columns specyfication to the list of colums for output
+                    tableDataColumnsSpec[i] = new DataColumnSpecCreator(columnsItarator.toString(), StringCell.TYPE).createSpec();
                     if(!cell.isMissing())
                     {
                         //TODO
                         //iterating every cell. Here should be anonymisation for that column
                         String value = cell.toString();
+                        //adding the value to the container
+                        container.addRowToTable(new DefaultRow(new RowKey(new String()+j), new DataCell[]{
+                            new StringCell(value.toString())
+                        }));
                         value.toString();
                     }
                 }
             }
+            i++;
         }
-        return new BufferedDataTable[] {};
+        //add all spec columns to the output 
+      
+        spec = new DataTableSpec(tableDataColumnsSpec);
+        
+        //close the contaier and return the output 
+        container.close();
+        
+        //marge all bufferedatatable to one output 
+        
+        BufferedDataTable[] output = {container.getTable()};
+        
+        return output;
     }
+    
 
     /**
      * {@inheritDoc}
