2ba0a22bb524a677bcbdbd2a73d647dbdddc97a7
Andrzej Dudek <andrzej@bigbee.eu>
2013-06-18 13:18:10 +0200

    project configuration and sanity checks before implementation

--- ic-depress-data-anonymisation/src/org/impressivecode/depress/data/anonymisation/EncryptionNodeModel.java
+++ ic-depress-data-anonymisation/src/org/impressivecode/depress/data/anonymisation/EncryptionNodeModel.java
@@ -1,7 +1,11 @@
 package org.impressivecode.depress.data.anonymisation;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.collection.ListCell;
+import org.knime.core.data.collection.SetCell;
 import org.knime.core.data.def.StringCell;
 import org.knime.core.node.NodeLogger;
 import org.knime.core.util.KnimeEncryption;
@@ -26,8 +30,11 @@ public class EncryptionNodeModel extends CryptoNodeModel {
 
             @Override
             protected DataCell transformCell(final DataCell dataCell) {
-                Preconditions.checkArgument(StringCell.TYPE.equals(dataCell.getType()),
-                        "Only string cells supported yet");
+                Preconditions.checkArgument(
+                    dataCell.getType().equals(StringCell.TYPE)
+                    || SetCell.class.isInstance(dataCell)
+                    || ListCell.class.isInstance(dataCell),
+                    "Cell type " + dataCell.getType().toString() + " is not supported");
 
                 try {
                     String origin = ((StringCell) dataCell).getStringValue();
