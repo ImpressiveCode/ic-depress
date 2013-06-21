434d403b3cce05083e349b458b034f8ab5de6fbd
Andrzej Dudek <andrzej@bigbee.eu>
2013-06-21 13:37:56 +0200

    unit tests progress

--- ic-depress-test/test/org/impressivecode/depress/test/data/anonymisation/DataAnonymisationTest.java
+++ ic-depress-test/test/org/impressivecode/depress/test/data/anonymisation/DataAnonymisationTest.java
@@ -22,25 +22,60 @@ import org.impressivecode.depress.data.anonymisation.ColumnCryptoTransformer;
 import org.junit.Before;
 import org.junit.Test;
 import org.knime.core.data.DataCell;
-import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.def.DefaultRow;
 import org.knime.core.data.def.StringCell;
+import org.knime.core.data.RowKey;
+import org.knime.core.node.BufferedDataContainer;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.ExecutionContext;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 /**
  * 
  * @author Andrzej Dudek, Wrocław Institute of Technology
- * @author Marek Majchrzak, Impressive Code
+ * 
  */
 
 public class DataAnonymisationTest {
+    
+    protected final BufferedDataTable createBufferedDataTable(
+            final ExecutionContext exec)
+            throws Exception {
+        // the DataTableSpec of the final table
+        DataTableSpec spec = new DataTableSpec(
+                new DataColumnSpecCreator("A", StringCell.TYPE).createSpec()
+                );
+        // init the container
+        BufferedDataContainer container = exec.createDataContainer(spec);
+
+        // add arbitrary number of rows to the container
+        DataRow firstRow = new DefaultRow(new RowKey("first"), new DataCell[]{
+            new StringCell("A1")
+        });
+        container.addRowToTable(firstRow);
+        DataRow secondRow = new DefaultRow(new RowKey("second"), new DataCell[]{
+            new StringCell("B1")
+        });
+        container.addRowToTable(secondRow);
+
+        // finally close the container and get the result table.
+        container.close();
+        BufferedDataTable result = container.getTable();
+        return result;
+    }
+        
 
     @Before
     public void setUp() throws Exception {
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldThrowNullPointerExceprion1() throws Exception {
+    public void paramethersShouldThrowNullPointerExceprion1() throws Exception {
         new ColumnCryptoTransformer(null, null) {
             @Override
             protected DataCell transformCell(DataCell dataCell) {
@@ -50,7 +85,7 @@ public class DataAnonymisationTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldThrowNullPointerExceprion2() throws Exception {
+    public void paramethersShouldThrowNullPointerExceprion2() throws Exception {
         DataTableSpec spec = new DataTableSpec(new DataColumnSpecCreator("string cell", StringCell.TYPE).createSpec());
 
         new ColumnCryptoTransformer(spec, null) {
@@ -60,4 +95,71 @@ public class DataAnonymisationTest {
             }
         };
     }
+
+    @Test(expected = NullPointerException.class)
+    public void paramethersShouldThrowNullPointerExceprion3() throws Exception {
+        String[] transforms = {};
+        new ColumnCryptoTransformer(null, transforms) {
+            @Override
+            protected DataCell transformCell(DataCell dataCell) {
+                return null;
+            }
+        };
+    }
+    
+    @Test
+    public void paramethersShouldSucceed() {
+        String[] transforms = {};
+        DataTableSpec spec = new DataTableSpec(new DataColumnSpecCreator("string cell", StringCell.TYPE).createSpec());
+
+        new ColumnCryptoTransformer(spec, transforms) {
+            @Override
+            protected DataCell transformCell(DataCell dataCell) {
+                return null;
+            }
+        };
+        
+        assertTrue(true);
+    }
+    
+    @Test
+    public void shouldEncryptText() throws Exception {
+     // given
+        ExecutionContext exec = mock(ExecutionContext.class);
+        BufferedDataContainer container = mock(BufferedDataContainer.class);
+        when(exec.createDataContainer(Mockito.any(DataTableSpec.class))).thenReturn(container);
+        BufferedDataTable data = createBufferedDataTable(exec);
+        DataTableSpec spec = new DataTableSpec(
+                        new DataColumnSpecCreator("A", StringCell.TYPE).createSpec()
+                        );
+        String[] transforms = spec.getColumnNames();
+        ColumnCryptoTransformer transformer = new ColumnCryptoTransformer(spec, transforms ) {
+            
+            @Override
+            protected DataCell transformCell(DataCell dataCell) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+        };
+
+        transformer.transform(data, exec);
+        
+//        BufferedDataContainer container = 
+//                mock(BufferedDataContainer.class);
+//        DataTable inTable = mock(DataTable.class);
+//        RowIterator iterator = mock(RowIterator.class);
+//        when(iterator.hasNext()).thenReturn(true, false);
+//        DataRow row = mock(DataRow.class);
+//        when(row.getCell(Mockito.eq(0))).thenReturn(Cells.stringCell("resource"));
+//        when(row.getCell(Mockito.eq(1))).thenReturn(Cells.stringSetCell(Sets.newHashSet("A", "B")));
+//        when(iterator.next()).thenReturn(row);
+//        when(inTable.iterator()).thenReturn(iterator);
+//        // when
+//        BufferedDataTable data = transformer.transform(container.getTable(), exec);
+//        // then
+//        assertThat(data).hasSize(1);
+//        assertThat(data.get(0).getResourceName()).isEqualTo("resource");
+//        assertThat(data.get(0).getMarkers()).containsOnly("A", "B");
+    }
+
 }
