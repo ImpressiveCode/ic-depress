79152f5afbba74d33aab3a8b0802739deb4589ec
Marek Majchrzak <majchmar@gmail.com>
2013-06-27 23:37:13 +0200

    fixing location of tests and plugin configuration

--- ic-depress-scm-test/test/org/impressivecode/depress/test/data/source/DataSourceAdapterMockClassProtectedAbstract.java
+++
@@ -1,7 +0,0 @@
-package org.impressivecode.depress.test.data.source;
-
-import java.io.IOException;
-
-public abstract class DataSourceAdapterMockClassProtectedAbstract {
-    protected abstract void Method2();
-}
