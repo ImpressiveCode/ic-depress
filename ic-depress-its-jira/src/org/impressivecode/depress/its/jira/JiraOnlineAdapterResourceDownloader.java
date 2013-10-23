89fb81c2252220d025bb5f3e4dd2bf0ef96cd37d
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2013-10-23 23:32:45 +0200

    resource downloader class
    added a class reponsible for downloading a JSON resource using URI to
    Jira and Client object

---
+++ ic-depress-its-jira/src/org/impressivecode/depress/its/jira/JiraOnlineAdapterResourceDownloader.java
@@ -0,0 +1,27 @@
+package org.impressivecode.depress.its.jira;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class JiraOnlineAdapterResourceDownloader {
+
+	public static Response getResource(Client client, String uri) throws Exception {
+		WebTarget target = client.target(uri);
+		Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+		// TODO consider handling no connection error
+		Response response = invocationBuilder.get();
+
+		// TODO check HTTP statuses that gives positive response
+		if (response.getStatus() != 200) {
+			Exception up = new Exception("a proper place to handle exceptions as wrong creditials etc.");
+			
+			throw up; // ;-)
+		}
+		
+		return response;
+	}
+
+}
