4a1e44f0dc1d608e3aac253f1c9207380d66dcbd
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2013-11-03 13:30:01 +0100

    amendments in integration test
    test has been amended to be downloading one Jira entry, parsing it to
    ITSData and verifing results with Hamcrest Matchers

--- ic-depress-its-jira/test/org/impressivecode/depress/its/jiraonline/JiraOnlineAdapterRsClientIntegrationTest.java
+++ ic-depress-its-jira/test/org/impressivecode/depress/its/jiraonline/JiraOnlineAdapterRsClientIntegrationTest.java
@@ -1,7 +1,17 @@
 package org.impressivecode.depress.its.jiraonline;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.impressivecode.depress.its.ITSDataType;
+import org.impressivecode.depress.its.ITSPriority;
+import org.impressivecode.depress.its.ITSResolution;
+import org.impressivecode.depress.its.ITSType;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,23 +36,47 @@ public class JiraOnlineAdapterRsClientIntegrationTest {
 	}
 
 	@Test
-	public void should_download_one_jira_issue() throws Exception {
+	public void should_download_and_parse_one_jira_issue() throws Exception {
 		String issueId = "HHH-8511";
-		String result = client.getIssueById(issueId);
+		String jiraEntry = wrapOneIssueToJSONArray(client.getIssueById(issueId));
 
-		String id = "\"id\":\"29618\"";
-		String summary = "\"summary\":\"\\\"Can't convert fromnull\\\" when inserting clob in Informix\"";
-		String priorityBlocker = "\"name\":\"Blocker\"";
-		String typeBug = "\"name\":\"Bug\"";
-		String reporterName = "\"name\":\"krasig\"";
-		String attachement = "\"filename\":\"SQLException.txt\"";
+		List<ITSDataType> entries = JiraOnlineParser.parse(jiraEntry);
 
-		assertTrue(result.contains(id));
-		assertTrue(result.contains(summary));
-		assertTrue(result.contains(priorityBlocker));
-		assertTrue(result.contains(typeBug));
-		assertTrue(result.contains(reporterName));
-		assertTrue(result.contains(attachement));
+		assertThat(entries, is(notNullValue()));
+		assertThat(entries.size(), is(equalTo(1)));
+
+		ITSDataType entry = entries.get(0);
+		assertThat(entry, is(notNullValue()));
+
+		String expectedId = "29618";
+		assertThat(entry.getIssueId(), is(equalTo(expectedId)));
+
+		String expectedSummary = "\"Can't convert fromnull\" when inserting clob in Informix";
+		assertThat(entry.getSummary(), is(equalTo(expectedSummary)));
+
+		ITSPriority expectedPriority = ITSPriority.BLOCKER;
+		assertThat(entry.getPriority(), is(equalTo(expectedPriority)));
+
+		ITSType expectedType = ITSType.BUG;
+
+		assertThat(entry.getType(), is(equalTo(expectedType)));
+		
+		String expectedReporter = "krasig";
+		assertThat(entry.getReporter(), is(equalTo(expectedReporter)));
+
+		String expectedLink = "https://hibernate.atlassian.net/rest/api/2/issue/29618";
+		assertThat(entry.getLink(), is(equalTo(expectedLink)));
+		
+		ITSResolution expectedResolution = ITSResolution.UNKNOWN;
+		assertThat(entry.getResolution(), is(equalTo(expectedResolution)));
+
+	}
+
+	private String wrapOneIssueToJSONArray(String jiraEntry) {
+		String pre = "{\"expand\":\"schema,names\",\"startAt\":0,\"maxResults\":50,\"total\":1,\"issues\":[";
+		String post = "]}";
+
+		return pre + jiraEntry + post;
 
 	}
 
