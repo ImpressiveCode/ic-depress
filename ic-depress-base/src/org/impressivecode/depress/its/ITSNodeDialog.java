cea4fd9dff8e7ebd057ccc36d5c01105e9273630
qmar <the.martines@gmail.com>
2014-01-20 23:37:07 +0100

    Removed workflow variables tab from ITS node dialogs

--- ic-depress-base/src/org/impressivecode/depress/its/ITSNodeDialog.java
+++ ic-depress-base/src/org/impressivecode/depress/its/ITSNodeDialog.java
@@ -1,3 +1,20 @@
+/*
+ImpressiveCode Depress Framework
+Copyright (C) 2013  ImpressiveCode contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.impressivecode.depress.its;
 
 import java.awt.event.ActionListener;
@@ -13,6 +30,13 @@ import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelInteger;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
+/**
+ * 
+ * @author Marcin Kunert, Wrocław University of Technology
+ * @author Krzysztof Kwoka, Wrocław University of Technology
+ * @author Bartosz Skuza, Wrocław University of Technology
+ * 
+ */
 public abstract class ITSNodeDialog extends DefaultNodeSettingsPane {
 
     public static final String CONNECTION_TAB_NAME = "Connection";
@@ -157,5 +181,10 @@ public abstract class ITSNodeDialog extends DefaultNodeSettingsPane {
 	}
 	
 	protected abstract SettingsModelInteger createThreadsCountSettings();
-
+	
+	@Override
+	protected void addFlowVariablesTab() {
+	    // NOOP the flow variables tab is not needed
+	}
+	
 }
