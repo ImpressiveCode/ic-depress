f75f7b2bf3ddabcb2a8b13674bf791ac9ee24572
zuzannapacholczyk <zuzannapacholczyk@wp.pl>
2014-07-04 11:24:04 +0200

    SVN GUI changes
    Changes in SVN GUI - added "Advanced" tab.

--- ic-depress-scm-svn/src/org/impressivecode/depress/scm/svn/SVNOfflineAdapterNodeDialog.java
+++ ic-depress-scm-svn/src/org/impressivecode/depress/scm/svn/SVNOfflineAdapterNodeDialog.java
@@ -19,7 +19,7 @@ package org.impressivecode.depress.scm.svn;
 
 import javax.swing.JFileChooser;
 
-import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+
 import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
 import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
 import org.knime.core.node.defaultnodesettings.DialogComponentLabel;
@@ -32,27 +32,34 @@ import org.knime.core.node.defaultnodesettings.SettingsModelString;
  * @author Marek Majchrzak, ImpressiveCode
  * @author Krystian Dabrowski, Capgemini Poland
  * 
- */
-public class SVNOfflineAdapterNodeDialog extends DefaultNodeSettingsPane {
-
+ **/
+//public class SVNOfflineAdapterNodeDialog extends DefaultNodeSettingsPane {
+public class SVNOfflineAdapterNodeDialog extends SVNNodeSettingsPane {
     protected SVNOfflineAdapterNodeDialog() {
         super();
 
         addDialogComponent(new DialogComponentFileChooser(new SettingsModelString(
                 SVNOfflineAdapterNodeModel.CFG_FILENAME, SVNOfflineAdapterNodeModel.FILENAME_DEFAULT),
                 SVNOfflineAdapterNodeModel.FILENAME_DEFAULT, JFileChooser.OPEN_DIALOG, false));
-
+        
+        // zuza
+        addDialogComponent(new DialogComponentString(new SettingsModelString(
+        		SVNOfflineAdapterNodeModel.CFG_STR, null), "Extension pattern:"));
+        
+        
         addDialogComponent(new DialogComponentLabel("Filter only file extensions:"));
         
         for(SVNFileExtensions supportedExtension:SVNFileExtensions.values()) {
         	SettingsModelBoolean settingsModelBoolean = new SettingsModelBoolean(supportedExtension.getConfigName(), supportedExtension.isDefaultSelected());
         	addDialogComponent(new DialogComponentBoolean(settingsModelBoolean, supportedExtension.getExtension()));	
         }
-        
+    	createNewGroup("Java");
         
         addDialogComponent(new DialogComponentString(new SettingsModelString(
                 SVNOfflineAdapterNodeModel.CFG_PACKAGENAME, SVNOfflineAdapterNodeModel.PACKAGENAME_DEFAULT),
-                "Package: "));
+                "Package prefix: "));
+        closeCurrentGroup();
         
     }
 }
+
