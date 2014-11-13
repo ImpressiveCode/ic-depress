2fb06d1086e06e6bc73c6f0236a4036baac50c52
mmacieksz <mac.sz430@gmail.com>
2014-11-13 22:26:10 +0100

    DEP-89 [DEP-94]
    Pregenerated code cleaned. Authors and open-source legal note added.

--- ic-depress-scm-endevor/src/org/impressivecode/depress/scm/endevor/EndevorParserNodeDialog.java
+++ ic-depress-scm-endevor/src/org/impressivecode/depress/scm/endevor/EndevorParserNodeDialog.java
@@ -1,37 +1,43 @@
+/*
+ImpressiveCode Depress Framework
+Copyright (C) 2013  ImpressiveCode contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.impressivecode.depress.scm.endevor;
 
+import javax.swing.JFileChooser;
+
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
-import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
-import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
+import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
 /**
- * <code>NodeDialog</code> for the "EndevorParser" Node.
  * 
- *
- * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
- * creation of a simple dialog with standard components. If you need a more 
- * complex dialog please derive directly from 
- * {@link org.knime.core.node.NodeDialogPane}.
+ * @author Alicja Bodys, Wroc³aw University of Technology
+ * @author Mateusz Leonowicz, Wroc³aw University of Technology
+ * @author Piotr Sas, Wroc³aw University of Technology
+ * @author Maciej Sznurowski, Wroc³aw University of Technology
  * 
- * @author zmwo-proj
  */
 public class EndevorParserNodeDialog extends DefaultNodeSettingsPane {
 
-    /**
-     * New pane for configuring EndevorParser node dialog.
-     * This is just a suggestion to demonstrate possible default dialog
-     * components.
-     */
     protected EndevorParserNodeDialog() {
         super();
         
-        addDialogComponent(new DialogComponentNumber(
-                new SettingsModelIntegerBounded(
-                    EndevorParserNodeModel.CFGKEY_COUNT,
-                    EndevorParserNodeModel.DEFAULT_COUNT,
-                    Integer.MIN_VALUE, Integer.MAX_VALUE),
-                    "Counter:", /*step*/ 1, /*componentwidth*/ 5));
+        addDialogComponent(new DialogComponentFileChooser(new SettingsModelString(EndevorParserNodeModel.CFG_SOURCEFILE_NAME, EndevorParserNodeModel.SOURCEFILE_NAME_DEFAULT), "",
+        		JFileChooser.OPEN_DIALOG, ".txt", ".log"));
                     
     }
 }
-
