183cc5f0a42cc7a10bc938b9522f17f57d0b8358
Marek Majchrzak <majchmar@gmail.com>
2014-11-19 22:11:25 +0100

    example update

--- DePress/app/examples/examples.js
+++ DePress/app/examples/examples.js
@@ -2,13 +2,36 @@
 
 angular.module('depressApp.examples', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/examples', {
-    templateUrl: 'examples/index.html',
-    controller: 'PageController'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/examples', {
+            templateUrl: 'examples/index.html',
+            controller: 'ExamplesController'
+        });
+    }])
 
-.controller('PageController', [function() {
+    .controller('ExamplesController', ['$anchorScroll', '$location', '$scope',
+        function ($anchorScroll, $location, $scope, $log) {
 
-}]);
+            $scope.gotoAnchor = function (x) {
+                if ($location.hash() !== x) {
+                    // set the $location.hash to `newHash` and
+                    // $anchorScroll will automatically scroll to it
+                    $location.hash(x);
+                } else {
+                    // call $anchorScroll() explicitly,
+                    // since $location.hash hasn't changed
+                    $anchorScroll();
+                }
+            };
+
+            $scope.showLightbox = function(x) {
+                event.preventDefault();
+                $(x).ekkoLightbox({
+                    'data-type' : 'image'
+                });
+            }
+        }
+
+
+
+    ]);
--- DePress/app/plugins/plugins.js
+++ DePress/app/plugins/plugins.js
@@ -3,8 +3,8 @@
 angular.module('depressApp.additional', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/additional', {
-    templateUrl: 'additional/index.html',
+  $routeProvider.when('/plugins', {
+    templateUrl: 'plugins/index.html',
     controller: 'PageController'
   });
 }])
