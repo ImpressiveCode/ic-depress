7940639d96d7672bbc2e1627e7742efc05255126
Marcin <choluj.marcin@hotmail.com>
2015-01-11 17:27:17 +0100

    DEP-82 parser

--- ic-depress-its-oschangemanagement/src/org/impressivecode/depress/its/oschangemanagement/parser/OsChangeManagementRationalAdapterParser.java
+++ ic-depress-its-oschangemanagement/src/org/impressivecode/depress/its/oschangemanagement/parser/OsChangeManagementRationalAdapterParser.java
@@ -4,10 +4,14 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import org.impressivecode.depress.its.ITSDataType;
 import org.impressivecode.depress.its.ITSPriority;
+import org.impressivecode.depress.its.ITSResolution;
+import org.impressivecode.depress.its.ITSStatus;
+import org.impressivecode.depress.its.ITSType;
 import org.impressivecode.depress.its.oschangemanagement.model.OsChangeManagementProject;
 import org.impressivecode.depress.its.oschangemanagement.model.rationaladapter.OsChangeManagementRationalAdapterIssueListItem;
 import org.impressivecode.depress.its.oschangemanagement.model.rationaladapter.OsChangeManagementRationalAdapterIssuesList;
@@ -16,7 +20,72 @@ import org.impressivecode.depress.its.oschangemanagement.model.rationaladapter.O
 
 public class OsChangeManagementRationalAdapterParser extends
 		OsChangeManagementAdapterParser {
+    private final HashMap<String, String[]> priorityMap;
+    private final HashMap<String, String[]> typeMap;
+    private final HashMap<String, String[]> resolutionMap;
+    private final HashMap<String, String[]> statusMap;
 	
+    public OsChangeManagementRationalAdapterParser(HashMap<String, String[]> priority, HashMap<String, String[]> type,
+            HashMap<String, String[]> resolution, HashMap<String, String[]> status) {
+        super();
+        this.priorityMap = priority;
+        this.typeMap = type;
+        this.resolutionMap = resolution;
+        this.statusMap = status;
+    }
+    
+    private ITSResolution parseResolutionFromMap(final String valueToParse) {
+        if (valueToParse == null) {
+            return ITSResolution.UNKNOWN;
+        }
+        for (String key : resolutionMap.keySet()) {
+            for (String value : resolutionMap.get(key)) {
+                if (valueToParse.equalsIgnoreCase(value))
+                    return ITSResolution.get(key);
+            }
+        }
+        return ITSResolution.UNKNOWN;
+    }
+
+    private ITSStatus parseStatusFromMap(final String valueToParse) {
+        if (valueToParse == null) {
+            return ITSStatus.UNKNOWN;
+        }
+        for (String key : statusMap.keySet()) {
+            for (String value : statusMap.get(key)) {
+                if (valueToParse.equalsIgnoreCase(value))
+                    return ITSStatus.get(key);
+            }
+        }
+        return ITSStatus.UNKNOWN;
+    }
+
+    private ITSType parseTypeFromMap(final String valueToParse) {
+        if (valueToParse == null) {
+            return ITSType.UNKNOWN;
+        }
+        for (String key : typeMap.keySet()) {
+            for (String value : typeMap.get(key)) {
+                if (valueToParse.equalsIgnoreCase(value))
+                    return ITSType.get(key);
+            }
+        }
+        return ITSType.UNKNOWN;
+    }
+
+    private ITSPriority parsePriorityFromMap(final String valueToParse) {
+        if (valueToParse == null) {
+            return ITSPriority.UNKNOWN;
+        }
+        for (String key : priorityMap.keySet()) {
+            for (String value : priorityMap.get(key)) {
+                if (valueToParse.equalsIgnoreCase(value))
+                    return ITSPriority.get(key);
+            }
+        }
+        return ITSPriority.UNKNOWN;
+    }
+    
 	@Override
 	public List<OsChangeManagementProject> getProjectList(String source) {
 		
@@ -56,10 +125,10 @@ public class OsChangeManagementRationalAdapterParser extends
 			its.setDescription(item.getTitle());
 			
 			its.setReporter(item.getFirstCreator());
-			its.setStatus(item.getStatusAsITS());
-			its.setType(item.getTypeAsITS());
-			its.setPriority(item.getPriorityAsITS());
-			its.setResolution(item.getResolutionAsITS());
+			its.setStatus(parseStatusFromMap(item.getStatus()));
+			its.setType(parseTypeFromMap(item.getType()));
+			its.setPriority(parsePriorityFromMap(item.getPriorityAsString()));
+			its.setResolution(parseResolutionFromMap(item.getResolution()));
 			try {
 				its.setUpdated(dateStringToObject(item.getModified()));
 			} catch (ParseException e) {
